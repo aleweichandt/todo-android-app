af09c5928d0e5dee63e4bc55764f0c80d372ceee
alew <ale.w.es@sync.money>
2021-02-08 19:19:55 +0100

    add menu option on todo list for new todos

--- features/todos/view/src/main/java/com/example/todo/todos/view/list/TodoListFragment.kt
+++ features/todos/view/src/main/java/com/example/todo/todos/view/list/TodoListFragment.kt
@@ -2,10 +2,12 @@ package com.example.todo.todos.view.list
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.example.todo.todos.view.R
 import com.example.todo.todos.view.databinding.FragmentTodoListBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -28,8 +30,17 @@ class TodoListFragment : Fragment() {
         with(bindings.swipeRefresh) {
             setOnRefreshListener(viewModel)
         }
+        bindings.toolbar.setOnMenuItemClickListener { onOptionsItemSelected(it) }
         observeViewModel()
         return bindings.root
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.menu_item_add_todo) {
+            // TODO navigate to form
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
