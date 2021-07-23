a0254c072f2e355e89be08d4eb2c3d34ec355d4d
alew <ale.w.es@sync.money>
2021-07-23 21:31:07 +0200

    add base data module for cache implementation

---
+++ libs/base/data/src/main/java/com/example/todo/base/data/cache/Cache.kt
@@ -0,0 +1,12 @@
+package com.example.todo.base.data.cache
+
+import com.example.todo.base.domain.IExceptionHandler
+
+class Cache(
+    private val exceptionHandler: IExceptionHandler
+) {
+    protected suspend fun <T> runQuery(query: suspend () -> T) =
+        query.runCatching { invoke() }
+            .onFailure { exceptionHandler.handle(it) }
+            .getOrNull()
+}
