39a64544a59254f396dd3887f53a163f8a10f129
alew <ale.w.es@sync.money>
2021-07-29 21:49:27 +0200

    fix invalid build configuration due to invalid gradle configuration on todosData module. Migrate dependency module to hilt

--- features/todos/data/src/main/java/com/example/todo/todos/data/di/TodoDataModule.kt
+++ features/todos/data/src/main/java/com/example/todo/todos/data/di/TodoDataModule.kt
@@ -4,9 +4,13 @@ import com.example.todo.todos.data.ITodoApi
 import com.example.todo.todos.data.MockTodosApi
 import dagger.Module
 import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 @Module
+@InstallIn(SingletonComponent::class)
 class TodoDataModule {
     @Provides
     @Singleton
