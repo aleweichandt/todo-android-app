cee164a5181b861e064d28c4b6c8a438aade71eb
alew <ale.w.es@sync.money>
2021-07-30 22:01:33 +0200

    add logs to track resource behaviour

--- features/todos/data/src/main/java/com/example/todo/todos/data/local/TodosCache.kt
+++ features/todos/data/src/main/java/com/example/todo/todos/data/local/TodosCache.kt
@@ -1,5 +1,6 @@
 package com.example.todo.todos.data.local
 
+import android.util.Log
 import com.example.todo.base.data.cache.Cache
 import com.example.todo.base.domain.IExceptionHandler
 import com.example.todo.todos.data.ITodoCache
@@ -11,16 +12,22 @@ class TodosCache(
     private val dao: TodoDao,
     exceptionHandler: IExceptionHandler
 ) : Cache(exceptionHandler), ITodoCache {
+    companion object {
+        private const val tag = "LOCAL-SOURCE"
+    }
     override suspend fun getAllTodos() = runQuery {
+        Log.d(tag, "Get All Todos")
         dao.getAll().map { it.toDomain() }
     }
 
     override suspend fun getTodoById(id: Long) = runQuery {
+        Log.d(tag, "Get Todo $id")
         dao.getById(id)?.toDomain()
     }
 
     override suspend fun storeAllTodos(todos: List<Todo>) {
         runQuery {
+            Log.d(tag, "Add All Todos")
             todos.map(Todo::toEntity).let {
                 dao.replaceAll(*it.toTypedArray())
             }
@@ -29,12 +36,14 @@ class TodosCache(
 
     override suspend fun storeTodo(todo: Todo) {
         runQuery {
+            Log.d(tag, "Add Todo ${todo.uuid}")
             dao.insert(todo.toEntity())
         }
     }
 
     override suspend fun deleteAllTodos() {
         runQuery {
+            Log.d(tag, "Delete All Todos")
             dao.deleteAll()
         }
     }
--- features/todos/data/src/main/java/com/example/todo/todos/data/remote/MockTodosApi.kt
+++ features/todos/data/src/main/java/com/example/todo/todos/data/remote/MockTodosApi.kt
@@ -1,26 +1,45 @@
 package com.example.todo.todos.data.remote
 
+import android.util.Log
 import com.example.todo.todos.data.ITodoApi
 import com.example.todo.todos.domain.model.Todo
+import kotlinx.coroutines.delay
 
 class MockTodosApi: ITodoApi {
+    companion object {
+        private const val tag = "REMOTE-SOURCE"
+        private const val FAKE_DELAY_MS = 2000L
+    }
     private val todos: MutableList<Todo> = mutableListOf(
         Todo("mock", "testt"),
         Todo("mock completed", "test", true)
     )
 
-    override suspend fun getAllTodos(): List<Todo>? = todos
+    override suspend fun getAllTodos(): List<Todo>? {
+        Log.d(tag, "Get All Todos")
+        delay(FAKE_DELAY_MS)
+        return todos
+    }
 
-    override suspend fun getTodoById(id: Long): Todo? = todos.find { it.uuid == id }
+    override suspend fun getTodoById(id: Long): Todo? {
+        Log.d(tag, "Get Todo $id")
+        delay(FAKE_DELAY_MS)
+        return todos.find { it.uuid == id }
+    }
 
-    override suspend fun addTodo(todo: Todo): Boolean? =
-        todos.add(todo)
+    override suspend fun addTodo(todo: Todo): Boolean? {
+        Log.d(tag, "Add Todo ${todo.uuid}")
+        return todos.add(todo)
+    }
 
-    override suspend fun deleteTodo(todo: Todo): Boolean? =
-        todos.remove(todo)
+    override suspend fun deleteTodo(todo: Todo): Boolean? {
+        Log.d(tag, "Delete Todo ${todo.uuid}")
+        return todos.remove(todo)
+    }
 
-    override suspend fun updateTodo(todo: Todo, update: Todo): Boolean? =
-        when (val index = todos.indexOf(todo)) {
+    override suspend fun updateTodo(todo: Todo, update: Todo): Boolean? {
+        Log.d(tag, "Update Todo ${todo.uuid}")
+        return when (val index = todos.indexOf(todo)) {
             -1 -> false
             else -> {
                 todos.removeAt(index)
@@ -28,4 +47,5 @@ class MockTodosApi: ITodoApi {
                 true
             }
         }
+    }
 }
