2bd270bdb811c32431064d953157d19a6a526668
alew <ale.w.es@sync.money>
2021-07-30 21:47:52 +0200

    migrate todo repository to provide by resource group

--- features/todos/domain/src/main/java/com/example/todo/todos/domain/repository/TodoRepository.kt
+++ features/todos/domain/src/main/java/com/example/todo/todos/domain/repository/TodoRepository.kt
@@ -1,27 +1,37 @@
 package com.example.todo.todos.domain.repository
 
+import com.example.todo.base.data.ResourceGroup
 import com.example.todo.todos.data.ITodoApi
+import com.example.todo.todos.data.ITodoCache
 import com.example.todo.todos.domain.model.Todo
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flowOf
 
 class TodoRepository(
+    private val localSource: ITodoCache,
     private val remoteSource: ITodoApi
 ) {
+    private val resourceGroup = ResourceGroup<Unit, Long, Todo>(
+        { remoteSource.getAllTodos() },
+        { localSource.getAllTodos() },
+        localSource::storeAllTodos,
+        { id, _ -> remoteSource.getTodoById(id) },
+        { id, _ -> localSource.getTodoById(id) },
+        localSource::storeTodo,
+        localSource::deleteAllTodos
+    )
 
     suspend fun getAllTodos(): Flow<List<Todo>?> =
-        flowOf(remoteSource.getAllTodos())
+        resourceGroup.query(Unit)
 
     suspend fun getTodoById(id: Long): Flow<Todo?> =
-        flowOf(remoteSource.getTodoById(id))
+        resourceGroup.queryByKey(id, Unit)
 
     suspend fun addTodo(todo: Todo): Boolean? =
-        remoteSource.addTodo(todo)
+        remoteSource.addTodo(todo).also { resourceGroup.evict() }
 
     suspend fun deleteTodo(todo: Todo): Boolean? =
-        remoteSource.deleteTodo(todo)
+        remoteSource.deleteTodo(todo).also { resourceGroup.evict() }
 
     suspend fun updateTodo(todo: Todo, update: Todo): Boolean? =
-        remoteSource.updateTodo(todo, update)
-
+        remoteSource.updateTodo(todo, update).also { resourceGroup.evict() }
 }
